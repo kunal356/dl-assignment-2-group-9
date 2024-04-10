4b0ca9b6772fab8bc33c95fd8bdaba93ddab57af
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2024-04-10 14:06:08 +0100

    Load data page done

--- pages/2_Load_Data_And_Overview.py
+++ pages/2_Load_Data_And_Overview.py
@@ -1,8 +1,35 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def display_graphs(df):
+    df = df.sample(1000)
+    st.write(df.shape)
+    all_columns_names = df.columns.to_list()
+    selected_column_names = st.multiselect("Select Columns to plot", all_columns_names)
+    cust_data = df[selected_column_names]
+    st.area_chart(cust_data)
+    st.bar_chart(cust_data)
+    st.line_chart(cust_data)
+
+    fig, ax = plt.subplots()
+    p1 = cust_data.plot(kind='hist', ax=ax)
+    st.write(p1)
+    st.pyplot(fig)
+    
+    fig1, ax1 = plt.subplots()
+    p2 = cust_data.plot(kind='box', ax=ax1)
+    st.write(p2)
+    st.pyplot(fig1)
+    
 
 def main():
-    st.title("Page inside page")
-
+    st.title("Load Data and Overview")
+    data = st.file_uploader("Upload your Dataset", type=['csv'])
+    if data is not None:
+        df = pd.read_csv(data)
+        st.write(df.head())
+        display_graphs(df)
 if __name__ == "__main__":
     main()
 
